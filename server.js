640593983184cf89867e4541c277991578b8c83c
Ben Burgos <ben.a.burgos@gmail.com>
2022-04-05 22:05:14 -0700

    adds file structure

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const express = require('express');
+const app = express();
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+});
+
+app.get('/drinks/:id', (req, res) => {
+    res.send(drinks[req.params.id])
+});
+
+app.listen(PORT, () => {
+    console.log(`
+    Are you locked up in a world
+    That's been planned out for you?
+    Are you feeling like a social -
+    Tool without a use?
+    Scream at me until my ears bleed.
+    I'm taking heed just for you.
+    `)
+});
