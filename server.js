f70f22833abe60337cc0a3b1bbb5b5d38b4c7d6c
Ben Burgos <ben.a.burgos@gmail.com>
2022-04-06 22:53:32 -0700

    adds initial food routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
 const drinks = require('./models/drinks.js')
+const food = require('./models/food.js')
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!')
@@ -17,6 +18,14 @@ app.get('/drinks/:id', (req, res) => {
     drinks[req.params.id].image = drinks[req.params.id].image.replace(/.jpg/, '.png')
     res.render('drinks_show.ejs', { drink: drinks[req.params.id]})
 });
+
+app.get('/food', (req, res) => {
+    res.render('food_index.ejs', { allFood: food })
+});
+
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', { food: food[req.params.id] })
+})
 
 app.listen(PORT, () => {
     console.log(`
